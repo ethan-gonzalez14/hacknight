fe9877c45af415482da0730897ad0855eb082791
chenjayden <jaydenchen08@gmai.com>
2025-05-31 21:03:18 -0400

    some stuff

---
+++ web/src/lib/Node.svelte
@@ -0,0 +1,23 @@
+<style> 
+    button {
+        height: 100px;
+        width: 100px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: transform 0.3s;
+    }
+    button:hover {
+    background-color: #1c86ee;
+    transform: scale(1.3);
+    }
+</style>
+
+<script>
+    export let text = "Click me";
+    export let onClick = () => {};
+  </script>
+  
+  <button on:click={onClick}>
+    {text}
+</button>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import Hello from "$lib/Hello.svelte";
+    import Node from "$lib/Node.svelte";
 </script>
 
 <Hello/>
 <Hello/>
+<Node/>
